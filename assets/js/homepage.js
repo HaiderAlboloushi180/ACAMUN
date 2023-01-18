b1e5db02f97ff933aad6ed0c695dde6854e3d4f2
HaiderAlboloushi180 <halbolou05@gmail.com>
2023-01-18 12:39:52 +0300

    less than a day

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -16,7 +16,7 @@ var x = setInterval(function () {
 
   // Display the result in the element with id="demo"
   document.getElementById("timer").innerHTML =
-    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    hours + "h " + minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
   if (distance < 0) {
